a0147af78f3f9c3b29525780fc99de2a961743e4
Gogamaga <jextailor@gmail.com>
2017-09-10 14:33:26 +0300

    home

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-hellozbzb
+// TAsk #1
+// for(var j = 1; j<=100; j++){
+// 	if(j%5 == 0){
+// 		console.log(j);
+// 	}
+// }
+
+// var i = 1;
+// while(i <= 100){
+//   if(i % 5 == 0){
+//     console.log(i);
+//   }
+//   i++;
+// };
+ //Task #2
+// var min = prompt('min');
+// var max = prompt('max');
+// for(var j = min; j <= max; j++){
+// 	if(j % 2 != 0){
+// 	console.log(j);
+// 	}
+// }
+
+// Task#3
+
+do{
+  var login = prompt('Login Please');
+}while(login != 'admin'){
+  if(login == 'admin'){
+  console.log('Hello Admin');
+}  
+}
